b79d581eade8d40ab975f281d7e034fd25b61e72
jasperjiang <jasper.jiang@pwc.com>
2020-07-17 11:43:23 +0800

    asdads

--- jobdsl2/src/jobs/jobs.groovy
+++ jobdsl2/src/jobs/jobs.groovy
@@ -1,5 +1,7 @@
+import com.demo.jobdsl.main.ComponentParser
+import com.demo.jobdsl.main.domain.Repository
 import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dslFactory = this
 
-//List<Repository> reposMetadata = new ComponentParser().parse(dslFactory)
+List<Repository> reposMetadata = new ComponentParser().parse(dslFactory)
